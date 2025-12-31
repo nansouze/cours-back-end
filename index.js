7bbdbfb652715d9089dc9794c7024f36acd57b57
François STABLE <francois.stable@3ds.com>
2025-12-31 17:28:15 +0100

    server with express

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur la page");
+});
+
+app.listen(PORT, () => {
+  console.log("Serveur démarré sur le port", PORT);
+});
