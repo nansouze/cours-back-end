82cbe28f5eb8a6f0df6ea734c0dce696f5240413
François STABLE <francois.stable@3ds.com>
2026-01-01 21:46:18 +0100

    middleware & ejs

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Bienvenue sur la page");
+  res.render("pages/home");
 });
 
 app.listen(PORT, () => {
