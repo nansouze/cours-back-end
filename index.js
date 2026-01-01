9c8c8f2f98c36b917aed8d2f15628fe4535b2cc1
François STABLE <francois.stable@3ds.com>
2026-01-01 22:41:44 +0100

    express static public folder & setup tailwind css

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import pagesRouter from "./routes/pages/index.js";
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 app.use("/", pagesRouter);
